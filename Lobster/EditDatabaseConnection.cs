4afdc2a4aa60f7aa4ef855be32b54b90778a798c
marshl <marshl@live.com.au>
2015-09-22 14:24:57 +1000

    Modified EditDatabaseConnection.cs to be StyleCop compliant;

--- Lobster/EditDatabaseConnection.cs
+++ Lobster/EditDatabaseConnection.cs
@@ -1,28 +1,49 @@
-﻿namespace Lobster
+﻿//-----------------------------------------------------------------------
+// <copyright file="EditDatabaseConnection.cs" company="marshl">
+// Copyright 2015, Liam Marshall, marshl.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------
+//
+//      'So!' said the Messenger. 'Then thou art the spokesman, old greybeard?'
+// 
+//      [ _The Lord of the Rings_, V/x: "The Black Gate Opens"]
+//
+//-----------------------------------------------------------------------
+namespace Lobster
 {
-    using Properties;
     using System;
     using System.IO;
     using System.Windows.Forms;
+    using Properties;
 
     /// <summary>
-    /// 'So!' said the Messenger. 'Then thou art the spokesman, old greybeard?'
-    /// [ _The Lord of the Rings_, V/x: "The Black Gate Opens"]
+    /// A form used to edit an old or new DatabaseConnection and its underlying ClobTypes.
     /// </summary>
     public class EditDatabaseConnection : EditCompositeObjectForm<DatabaseConnection>
     {
         /// <summary>
-        /// 
+        /// Initializes a new instance of the <see cref="EditDatabaseConnection"/> class.
         /// </summary>
-        /// <param name="_original"></param>
-        /// <param name="_newConnection"></param>
-        public EditDatabaseConnection(DatabaseConnection _original, bool _newConnection) : base(_original, _newConnection)
+        /// <param name="originalConnection">The connection to edit.</param>
+        /// <param name="isNewConnection">Whether the DatabaseConnection is new or old.</param>
+        public EditDatabaseConnection(DatabaseConnection originalConnection, bool isNewConnection)
+            : base(originalConnection, isNewConnection)
         {
-
         }
 
         /// <summary>
-        /// 
+        /// Override for the base InitializeComponent method, with changes specific for editing DatabaseConnections.
         /// </summary>
         protected override void InitializeComponent()
         {
@@ -35,7 +56,8 @@
         }
 
         /// <summary>
-        /// 
+        /// Override for the bas PopulateSubItemList. Populates the sub item list with ClobTypes 
+        /// from the working DatabaseConnection
         /// </summary>
         protected override void PopulateSubItemList()
         {
@@ -52,9 +74,9 @@
         }
 
         /// <summary>
-        /// 
+        /// Validates that the changes made to the ClobType are valid.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>True if the ClobType is valid.</returns>
         protected override bool ValidateChanges()
         {
             if (this.WorkingObject.CodeSource == null
@@ -90,9 +112,9 @@
         }
 
         /// <summary>
-        /// 
+        /// Saves changes to the ClobType out to file.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>True if the changes were succcessfully applied, otherwise false.</returns>
         protected override bool ApplyChanges()
         {
             try
@@ -113,10 +135,11 @@
         }
 
         /// <summary>
-        /// 
+        /// The override handler for the add sub item button click event.
+        /// OPens an instance of the EditClobType form with a new ClobType as the context.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         protected override void addSubItemButton_click(object sender, EventArgs e)
         {
             ClobType clobType = new ClobType();
@@ -132,10 +155,11 @@
         }
 
         /// <summary>
-        /// 
+        /// The override event handler for the remove sub item button click.
+        /// Removes the selected ClobType and refreshes the list.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         protected override void removeSubItemButton_Click(object sender, EventArgs e)
         {
             if (this.subItemListView.SelectedItems.Count == 0)
@@ -151,10 +175,11 @@
         }
 
         /// <summary>
-        /// 
+        /// The override for the edit sub item button event handler.
+        /// Creates a EditClobType form with the selected CLobType as the context.
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
         protected override void editSubItemButton_Click(object sender, EventArgs e)
         {
             if (this.subItemListView.SelectedItems.Count == 0)
@@ -167,7 +192,7 @@
             ClobType clobType = this.WorkingObject.ClobTypeList[clobTypeIndex];
 
             EditClobType editForm = new EditClobType(clobType, false);
-            DialogResult ctResult = editForm.ShowDialog();
+            DialogResult result = editForm.ShowDialog();
             this.WorkingObject.ClobTypeList[clobTypeIndex] = editForm.OriginalObject;
             this.PopulateSubItemList();
         }
