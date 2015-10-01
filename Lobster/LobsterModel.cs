4c9936a6deec2ebd740ed19fd6262a68edd24bc3
marshl <marshl@live.com.au>
2015-10-01 20:56:29 +1000

    Fixed merging errors;

--- Lobster/LobsterModel.cs
+++ Lobster/LobsterModel.cs
@@ -27,6 +27,7 @@ namespace Lobster
     using System;
     using System.Collections.Generic;
     using System.Data;
+    using System.Data.Common;
     using System.Diagnostics;
     using System.IO;
     using System.Linq;
@@ -77,7 +78,7 @@ namespace Lobster
         /// <param name="clobFile">The file to update.</param>
         public void SendUpdateClobMessage(ClobFile clobFile)
         {
-            OracleConnection con = OpenConnection(this.CurrentConnection);
+            DbConnection con = OpenConnection(this.CurrentConnection);
             bool result;
             if (con == null)
             {
@@ -115,7 +116,7 @@ namespace Lobster
         /// <returns>True if the file was inserted, otherwise false.</returns>
         public bool SendInsertClobMessage(ClobFile clobFile, Table table, string mimeType)
         {
-            OracleConnection con = OpenConnection(this.CurrentConnection);
+            DbConnection con = OpenConnection(this.CurrentConnection);
             if (con == null)
             {
                 return false;
@@ -133,7 +134,7 @@ namespace Lobster
         /// <returns>The path of the resulting file.</returns>
         public string SendDownloadClobDataToFileMessage(ClobFile clobFile)
         {
-            OracleConnection con = OpenConnection(this.CurrentConnection);
+            DbConnection con = OpenConnection(this.CurrentConnection);
             if (con == null)
             {
                 return null;
@@ -154,7 +155,7 @@ namespace Lobster
         public bool SetDatabaseConnection(DatabaseConnection connection)
         {
             MessageLog.LogInfo("Changing connection to " + connection.Name);
-            using (OracleConnection con = OpenConnection(connection))
+            using (DbConnection con = OpenConnection(connection))
             {
                 if (con == null)
                 {
@@ -275,7 +276,7 @@ namespace Lobster
         /// </summary>
         /// <param name="config">The connection configuration settings to use.</param>
         /// <returns>A new connectionif it opened successfully, otherwise null.</returns>
-        private static OracleConnection OpenConnection(DatabaseConnection config)
+        private static DbConnection OpenConnection(DatabaseConnection config)
         {
             try
             {
@@ -340,7 +341,7 @@ namespace Lobster
         /// </summary>
         private void GetDatabaseFileLists()
         {
-            OracleConnection con = OpenConnection(this.CurrentConnection);
+            DbConnection con = OpenConnection(this.CurrentConnection);
             if (con == null)
             {
                 MessageLog.LogError("Connection failed, cannot diff files with database.");
@@ -367,12 +368,12 @@ namespace Lobster
         /// <param name="clobFile">The file to insert into the database.</param>
         /// <param name="con">The Oracle connction to use.</param>
         /// <returns>True if the file was updated successfully, otherwise false.</returns>
-        private bool UpdateDatabaseClob(ClobFile clobFile, OracleConnection con)
+        private bool UpdateDatabaseClob(ClobFile clobFile, DbConnection con)
         {
             Debug.Assert(clobFile.IsSynced, "A clobfile must be synchronised with the database to reclob it");
 
-            OracleTransaction trans = con.BeginTransaction();
-            OracleCommand command = con.CreateCommand();
+            DbTransaction trans = con.BeginTransaction();
+            DbCommand command = con.CreateCommand();
             Table table = clobFile.DatabaseFile.ParentTable;
 
             try
@@ -438,7 +439,7 @@ namespace Lobster
         /// <param name="clobFile">The local file which will have its data bound to the query.</param>
         /// <param name="table">The table that the file will be added to.</param>
         /// <param name="mimeType">The mime type the file will be added as, if any.</param>
-        private void AddFileDataParameter(OracleCommand command, ClobFile clobFile, Table table, string mimeType)
+        private void AddFileDataParameter(DbCommand command, ClobFile clobFile, Table table, string mimeType)
         {
             Debug.Assert(clobFile.LocalFile != null, "Adding the contents of a local file to a command requires the file to be local");
 
@@ -453,13 +454,27 @@ namespace Lobster
                     x => x.ColumnPurpose == Column.Purpose.CLOB_DATA
                         && (mimeType == null || x.MimeTypeList.Contains(mimeType)));
 
+
+                DbParameter param = command.CreateParameter();
+                param.ParameterName = "data";
+
                 // Binary mode
                 if (column.DataType == Column.Datatype.BLOB)
                 {
                     byte[] fileData = new byte[fs.Length];
                     fs.Read(fileData, 0, Convert.ToInt32(fs.Length));
-                    OracleParameter param = command.Parameters.Add("data", OracleDbType.Blob);
+
                     param.Value = fileData;
+
+                    if (command is OracleCommand)
+                    {
+                        OracleParameter op = (OracleParameter)param;
+                        op.OracleDbType = OracleDbType.Blob;
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
                 }
                 else
                 {
@@ -467,9 +482,20 @@ namespace Lobster
                     StreamReader sr = new StreamReader(fs);
                     string contents = sr.ReadToEnd();
                     contents += this.GetClobFooterMessage(mimeType);
-                    OracleDbType insertType = column.DataType == Column.Datatype.CLOB ? OracleDbType.Clob : OracleDbType.XmlType;
-                    command.Parameters.Add("data", insertType, contents, ParameterDirection.Input);
+                    
+                    param.Value = contents;
+                    if ( command is OracleCommand )
+                    {
+                        OracleParameter op = (OracleParameter)param;
+                        op.OracleDbType = column.DataType == Column.Datatype.XMLTYPE ? OracleDbType.XmlType : OracleDbType.Clob;
+                    }
+                    else
+                    {
+                        throw new NotImplementedException();
+                    }
                 }
+
+                command.Parameters.Add(param);
             }
         }
 
@@ -481,12 +507,12 @@ namespace Lobster
         /// <param name="mimeType">The mimetype to insert the file as.</param>
         /// <param name="con">The Oracle connection to use.</param>
         /// <returns>True if the file was inserted successfully, otherwise false.</returns>
-        private bool InsertDatabaseClob(ClobFile clobFile, Table table, string mimeType, OracleConnection con)
+        private bool InsertDatabaseClob(ClobFile clobFile, Table table, string mimeType, DbConnection con)
         {
             Debug.Assert(clobFile.IsLocalOnly, "The file must be local only for it to be inserted into the database");
 
-            OracleCommand command = con.CreateCommand();
-            OracleTransaction trans = con.BeginTransaction();
+            DbCommand command = con.CreateCommand();
+            DbTransaction trans = con.BeginTransaction();
             string mnemonic = this.ConvertFilenameToMnemonic(clobFile, table, mimeType);
 
             if (table.ParentTable != null)
@@ -558,13 +584,12 @@ namespace Lobster
         /// Downloads the current content of a file on the database to a local temp file.
         /// </summary>
         /// <param name="clobFile">The file to download.</param>
-        /// <param name="con">The Oracle connection to use.</param>
         /// <param name="filename">The filepath to download the data into.</param>
         /// <returns>The path of the temporary file, if it exists.</returns>
-        private bool DownloadClobDataToFile(ClobFile clobFile, OracleConnection con, string filename)
+        private bool DownloadClobDataToFile(ClobFile clobFile, DbConnection con, string filename)
         {
             Debug.Assert(clobFile.DatabaseFile != null, "The ClobFile must be on the database for it to be downloaded");
-            OracleCommand command = con.CreateCommand();
+            DbCommand command = con.CreateCommand();
 
             Table table = clobFile.DatabaseFile.ParentTable;
             Column column;
@@ -582,14 +607,17 @@ namespace Lobster
 
             try
             {
-                OracleDataReader reader = command.ExecuteReader();
+                DbDataReader reader = command.ExecuteReader();
 
                 if (reader.Read())
                 {
                     if (column.DataType == Column.Datatype.BLOB)
                     {
-                        OracleBlob blob = reader.GetOracleBlob(0);
-                        File.WriteAllBytes(filename, blob.Value);
+                        //OracleBlob blob = reader.//reader.GetOracleBlob(0);
+                        //File.WriteAllBytes(filepath, blob.Value);
+                        byte[] b = new Byte[(reader.GetBytes(0, 0, null, 0, int.MaxValue))];
+                        reader.GetBytes(0, 0, b, 0, b.Length);
+                        File.WriteAllBytes(filename, b);
                     }
                     else
                     {
@@ -597,13 +625,16 @@ namespace Lobster
                         Column clobColumn = clobFile.DatabaseFile.ParentTable.Columns.Find(x => x.ColumnPurpose == Column.Purpose.CLOB_DATA);
                         if (clobColumn.DataType == Column.Datatype.CLOB)
                         {
-                            OracleClob clob = reader.GetOracleClob(0);
-                            result = clob.Value;
+                            //OracleClob clob = reader.GetOracleClob(0);
+                            //reader.GetBytes(0,0,null,0,)
+                            result = reader.GetString(0);
+                            //result = clob.Value;
                         }
                         else
                         {
-                            OracleXmlType xml = reader.GetOracleXmlType(0);
-                            result = xml.Value;
+                            //OracleXmlType xml = reader.GetOracleXmlType(0);
+                            //result = xml.Value;
+                            result = reader.GetString(0);
                         }
 
                         StreamWriter streamWriter = File.AppendText(filename);
@@ -654,15 +685,15 @@ namespace Lobster
         /// </summary>
         /// <param name="clobDir">The directory to get the file lists for.</param>
         /// <param name="con">The Oracle connection to use.</param>
-        private void GetDatabaseFileListForDirectory(ClobDirectory clobDir, OracleConnection con)
+        private void GetDatabaseFileListForDirectory(ClobDirectory clobDir, DbConnection con)
         {
             clobDir.DatabaseFileList = new List<DBClobFile>();
             ClobType ct = clobDir.ClobType;
             foreach (Table table in ct.Tables)
             {
-                OracleCommand command = con.CreateCommand();
+                DbCommand command = con.CreateCommand();
                 command.CommandText = table.GetFileListCommand();
-                OracleDataReader reader;
+                DbDataReader reader;
                 try
                 {
                     reader = command.ExecuteReader();
