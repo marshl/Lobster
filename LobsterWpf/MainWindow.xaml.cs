f6dda173c559eb2548a897f4f8edebf09fb8d448
marshl <marshl@live.com.au>
2015-11-03 13:48:15 +1100

    Added WPF project to solution with GUI concepts;

---
+++ LobsterWpf/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LobsterWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ClobType> ClobTypes { get; }
+
+        public class ClobType
+        {
+            public string Name { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.ClobTypes = new ObservableCollection<ClobType>();
+            this.ClobTypes.Add(new ClobType() { Name = "Hello" });
+            this.ClobTypes.Add(new ClobType() { Name = "World" });
+
+            this.DataContext = this;
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectionListWindow window = new ConnectionListWindow();
+            window.ShowDialog();
+        }
+    }
+}
