f8956e053be9cc26347328fdb3e121a29b94cfb9
marshl <marshl@live.com.au>
2015-11-18 16:14:01 +1100

    Added basic Notification Window toaster popup;

---
+++ LobsterWpf/NotificationWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LobsterWpf
+{
+    /// <summary>
+    /// Interaction logic for NotificationWindow.xaml
+    /// </summary>
+    public partial class NotificationWindow : Window
+    {
+        public NotificationWindow()
+        {
+            InitializeComponent();
+
+            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() =>
+            {
+                System.Drawing.Rectangle workingArea = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
+                Matrix transform = PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice;
+                Point corner = transform.Transform(new Point(workingArea.Right, workingArea.Bottom));
+
+                this.Left = corner.X - this.ActualWidth - 100;
+                this.Top = corner.Y - this.ActualHeight;
+            }));
+        }
+
+        private void Storyboard_Completed(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
