05ffb30845bed74603ac57821249bee4b7e83f7e
marshl <marshl@live.com.au>
2015-12-19 21:33:06 +1100

    StyleCopped the MimeTypeSelectorWindow.xamls.cs file;

--- LobsterWpf/MimeTypeSelectorWindow.xaml.cs
+++ LobsterWpf/MimeTypeSelectorWindow.xaml.cs
@@ -1,52 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-
+﻿//-----------------------------------------------------------------------
+// <copyright file="MimeTypeSelectorWindow.xaml.cs" company="marshl">
+// Copyright 2015, Liam Marshall, marshl.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//-----------------------------------------------------------------------
 namespace LobsterWpf
 {
+    using System.Collections.ObjectModel;
+    using System.Windows;
+
     /// <summary>
-    /// Interaction logic for MimeTypeSelectorWindow.xaml
+    /// The window for the user to select a mime type to insert a file as.
     /// </summary>
     public partial class MimeTypeSelectorWindow : Window
     {
-        public string MessageLabel { get; set; }
-
-        public ObservableCollection<string> MimeTypeList { get; set; }
-
-        public string SelectedMimeType
-        {
-            get
-            {
-                return (string)this.mimeTypeListBox.SelectedItem;
-            }
-        }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MimeTypeSelectorWindow"/> class.
+        /// </summary>
+        /// <param name="filename">The name of the file that the mime type is being selected for.</param>
+        /// <param name="mimeTypes">The mime type the user can select from.</param>
         public MimeTypeSelectorWindow(string filename, string[] mimeTypes)
         {
             this.MessageLabel = $"Please select a mime type for {System.IO.Path.GetFileName(filename)}:";
             this.MimeTypeList = new ObservableCollection<string>(mimeTypes);
             this.DataContext = this;
 
-            InitializeComponent();
+            this.InitializeComponent();
         }
 
-        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Gets the label to display to the user.
+        /// </summary>
+        public string MessageLabel { get; }
+
+        /// <summary>
+        /// Gets the mime types that the user can select from.
+        /// </summary>
+        public ObservableCollection<string> MimeTypeList { get; }
+
+        /// <summary>
+        /// Gets the mime type that the user has selected.
+        /// </summary>
+        public string SelectedMimeType
+        {
+            get
+            {
+                return this.mimeTypeListBox.SelectedItem as string;
+            }
+        }
+
+        /// <summary>
+        /// The event for when the cancel button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
         {
             this.DialogResult = false;
             this.Close();
         }
 
-        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// The event for when the accept button is clicked.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The event arguments.</param>
+        private void AcceptButton_Click(object sender, RoutedEventArgs e)
         {
             if (this.SelectedMimeType != null)
             {
