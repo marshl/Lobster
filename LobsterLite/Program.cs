64463927b69273c639062c2660d29b8888a939f1
marshl <marshl@live.com.au>
2016-06-22 17:58:28 +1000

    Added the LobsterLite project;

---
+++ LobsterLite/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LobsterModel;
+
+namespace LobsterLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseConfig config = DatabaseConfig.LoadDatabaseConfig()
+            DatabaseConnection connection = new DatabaseConnection()
+        }
+    }
+}
