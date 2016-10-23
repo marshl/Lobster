4469b80f3470695fb309cda45257e70109333cb1
marshl <marshl@live.com.au>
2016-10-24 08:15:44 +1100

    Added DirectoryWatcherView.cs

---
+++ LobsterWpf/ViewModels/DirectoryWatcherView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LobsterModel;
+
+namespace LobsterWpf.ViewModels
+{
+    public class DirectoryWatcherView
+    {
+        public DirectoryWatcherView(DirectoryWatcher watcher)
+        {
+            this.Watcher = watcher;
+        }
+
+        public DirectoryWatcher Watcher { get; }
+    }
+}
