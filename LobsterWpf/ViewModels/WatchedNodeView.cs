48e38523ca7780d32fda469e6b64b4f0dba2f9ab
marshl <marshl@live.com.au>
2016-10-25 22:39:28 +1100

    Added WatchedNodeView.cs

---
+++ LobsterWpf/ViewModels/WatchedNodeView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LobsterModel;
+
+namespace LobsterWpf.ViewModels
+{
+    public class WatchedNodeView
+    {
+        public WatchedNode BaseNode { get; }
+
+        public WatchedNodeView(WatchedNode node)
+        {
+            this.BaseNode = node;
+        }
+    }
+}
